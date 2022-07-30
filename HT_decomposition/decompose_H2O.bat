33be8c27ed5200041234cb651542e2748c17a681
laiadc <laia.d.c@hotmail.com>
2022-07-30 19:30:48 +0200

    Adding new params Ising model

--- HT_decomposition/decompose_H2O.bat
+++ HT_decomposition/decompose_H2O.bat
@@ -1,6 +1,6 @@
 @echo off
 
-FOR /F "delims=," %%x in (angles_H2O.csv) DO (
+FOR /F "delims=," %%x in (angles_H2O_new_params.csv) DO (
 	cd C:\cabal\bin
 	echo %%x>> %1
 	gridsynth %%x>> %1
--- HT_decomposition/decompose_LiH.bat
+++ HT_decomposition/decompose_LiH.bat
@@ -1,6 +1,6 @@
 @echo off
 
-FOR /F "delims=," %%x in (angles_LiH.csv) DO (
+FOR /F "delims=," %%x in (angles_LiH_new_params.csv) DO (
 	cd C:\cabal\bin
 	echo %%x>> %1
 	gridsynth %%x>> %1
